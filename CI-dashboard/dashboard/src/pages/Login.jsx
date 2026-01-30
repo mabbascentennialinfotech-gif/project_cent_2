6fb5324d4dccb20dbbd59acde89ad9f9adad4c4f
mabbascentennialinfotech-gif <mabbas.centennialinfotech@gmail.com>
2026-01-31 03:51:16 +0530

    new login update

--- CI-dashboard/dashboard/src/pages/Login.jsx
+++ CI-dashboard/dashboard/src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://backend-5bm0.onrender.com/api/auth/login",
         form,
       );
       localStorage.setItem("token", res.data.token);
--- CI-dashboard/dashboard/src/pages/Signup.jsx
+++ CI-dashboard/dashboard/src/pages/Signup.jsx
@@ -14,7 +14,10 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", form);
+      await axios.post(
+        "https://backend-5bm0.onrender.com/api/auth/signup",
+        form,
+      );
       navigate("/login");
     } catch (err) {
       alert(err.response?.data?.message || "Signup failed");
