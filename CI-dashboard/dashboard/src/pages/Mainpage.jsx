8c410767d253965b9867cb2c719e787d404bb0c0
abbascentennia <mabbas.centennialinfotech@gmail.com>
2026-01-31 15:21:21 +0530

    Update Mainpage.jsx

--- CI-dashboard/dashboard/src/pages/Mainpage.jsx
+++ CI-dashboard/dashboard/src/pages/Mainpage.jsx
@@ -18,8 +18,8 @@ import {
 } from "lucide-react";
 // import "/index.css";
 
-const API_URL = "http://localhost:5000/api";
-// const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const MONTHS = [
   "January",
