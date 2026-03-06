92a5bec4a8b1d9fbadeb67703c7080155898abe8
abbascentennia <mabbas.centennialinfotech@gmail.com>
2026-03-07 01:16:36 +0530

    Update index.js

--- CI-dashboard/backend/index.js
+++ CI-dashboard/backend/index.js
@@ -12,11 +12,13 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    "https://frontenddashboard-wcz6.onrender.com",
+    "https://jobportal.centennialinfotech.com"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
